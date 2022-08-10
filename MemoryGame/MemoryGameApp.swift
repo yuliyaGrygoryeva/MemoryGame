403425170470e825651d4132f78b1eb42f139d95
Yuliya Grygoryeva <jgrygoryeva@gmail.com>
2022-08-09 18:22:32 -0700

    initial commit

---
+++ MemoryGame/MemoryGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemoryGameApp.swift
+//  MemoryGame
+//
+//  Created by Yuliya  on 8/9/22.
+//
+
+import SwiftUI
+
+@main
+struct MemoryGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
