8a52c1b22f512dec0c36e73958f665dab1af1f92
Yuliya Grygoryeva <jgrygoryeva@gmail.com>
2022-08-10 11:18:28 -0700

    working on grid

--- MemoryGame/Views/Grid.swift
+++ MemoryGame/Views/Grid.swift
@@ -11,6 +11,11 @@ struct Grid<Item, ItemView>: View {
     var items: [Item]
     var viewForItem: (Item) -> ItemView
     
+    init(items: [Item], viewForItem: @escaping (Item) -> ItemView){
+        self.items = items
+        self.viewForItem = viewForItem
+    }
+    
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
