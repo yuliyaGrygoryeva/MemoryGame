a6b4d84b8bb353bf980a283a47a6f60d2723c1dc
Yuliya Grygoryeva <jgrygoryeva@gmail.com>
2022-08-09 23:35:28 -0700

    Starting work with Grid View

--- MemoryGame/Views/EmojiMemoryGameView.swift
+++ MemoryGame/Views/EmojiMemoryGameView.swift
@@ -12,14 +12,12 @@ struct EmojiMemoryGameView: View {
     @StateObject var viewModel: EmojiMemoryGame
     
     var body: some View {
-        HStack {
-            
-            ForEach(viewModel.cards) { card in
+        Grid(items: viewModel.cards) { card in
                 CardView(card: card).onTapGesture {
                     viewModel.choose(card: card)
                 }
             }
-        }
+        
         .padding()
         .foregroundColor(Color.orange)
         .font(Font.largeTitle)
@@ -30,20 +28,35 @@ struct CardView: View {
     var card: MemoryGame<String>.Card
     
     var body: some View {
+        GeometryReader { geometry in
+            self.body(for: geometry.size)
+        }
+    }
+    
+    func body(for size: CGSize) -> some View {
         ZStack {
             if card.isFaceUp {
-                RoundedRectangle(cornerRadius: 10.0).fill(Color.white)
-                RoundedRectangle(cornerRadius: 10.0).stroke(lineWidth: 3)
+                RoundedRectangle(cornerRadius: cornerRadius).fill(Color.white)
+                RoundedRectangle(cornerRadius: cornerRadius).stroke(lineWidth: edgeLineWidth)
                 Text(card.content)
             } else {
-                RoundedRectangle(cornerRadius: 10.0).fill()
+                RoundedRectangle(cornerRadius: cornerRadius).fill()
             }
         }
+        .font(Font.system(size: fontSize(for: size)))
         
     }
+    
+    // MARK: - Drawing Constants
+    let cornerRadius: CGFloat = 10.0
+    let edgeLineWidth: CGFloat = 3
+    let fontScaleFactor: CGFloat = 0.75
+    
+    func fontSize(for size: CGSize) -> CGFloat {
+        min(size.width, size.height) * fontScaleFactor
+    }
 }
-
-
+    
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         EmojiMemoryGameView(viewModel: EmojiMemoryGame())
