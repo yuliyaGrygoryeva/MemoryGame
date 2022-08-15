bd505c63873430bf175d835ad9f2374379989e17
Yuliya Grygoryeva <jgrygoryeva@gmail.com>
2022-08-14 20:23:03 -0700

    cardify modifier now works

--- MemoryGame/Views/EmojiMemoryGameView.swift
+++ MemoryGame/Views/EmojiMemoryGameView.swift
@@ -35,26 +35,21 @@ struct CardView: View {
         }
     }
     
+    @ViewBuilder
     private func body(for size: CGSize) -> some View {
+        if card.isFaceUp || !card.isMatched {
         ZStack {
-            if card.isFaceUp {
-                RoundedRectangle(cornerRadius: cornerRadius).fill(Color.white)
-                RoundedRectangle(cornerRadius: cornerRadius).stroke(lineWidth: edgeLineWidth)
-                Text(card.content)
-            } else {
-                if !card.isMatched {
-                RoundedRectangle(cornerRadius: cornerRadius).fill()
-                }
-            }
+            Pie(startAngle: Angle.degrees(0-90), endAngle: Angle.degrees(110-90), clockwise: true)
+                .padding(5).opacity((0.4))
+            Text(card.content)
+                .font(Font.system(size: fontSize(for: size)))
         }
-        .font(Font.system(size: fontSize(for: size)))
-        
+        .cardify(isFaceUp: card.isFaceUp)
+        }
     }
     
     // MARK: - Drawing Constants
-    private let cornerRadius: CGFloat = 10.0
-    private let edgeLineWidth: CGFloat = 3
-    private let fontScaleFactor: CGFloat = 0.75
+    private let fontScaleFactor: CGFloat = 0.7
     
     private func fontSize(for size: CGSize) -> CGFloat {
         min(size.width, size.height) * fontScaleFactor
@@ -63,6 +58,10 @@ struct CardView: View {
     
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        EmojiMemoryGameView(viewModel: EmojiMemoryGame())
+        let game = EmojiMemoryGame()
+        game.choose(card: game.cards[0])
+        return EmojiMemoryGameView(viewModel: game)
     }
 }
+
+
