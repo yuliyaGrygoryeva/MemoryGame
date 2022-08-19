5665736632a6fac64d41d2059f0e90889ac75ef0
Yuliya Grygoryeva <jgrygoryeva@gmail.com>
2022-08-19 00:25:19 -0700

    Animation need to be fixed

--- MemoryGame/Views/EmojiMemoryGameView.swift
+++ MemoryGame/Views/EmojiMemoryGameView.swift
@@ -8,21 +8,31 @@
 import SwiftUI
 
 struct EmojiMemoryGameView: View {
-    
+//    @ObservedObject var viewModel: EmojiMemoryGame
     @StateObject var viewModel: EmojiMemoryGame
     
     var body: some View {
-        Grid(viewModel.cards) { card in
+        VStack {
+            Grid(viewModel.cards) { card in
                 CardView(card: card).onTapGesture {
-                    viewModel.choose(card: card)
+                    withAnimation(.linear(duration: 0.75)) {
+                        viewModel.choose(card: card)
+                    }
                 }
                 .shadow(color: Color.black.opacity(0.2), radius: 5, x: 0, y: 2)
                 .padding(5)
             }
-        
-        .padding()
-        .foregroundColor(Color.orange)
-        .font(Font.largeTitle)
+            
+            .padding()
+            .foregroundColor(Color.orange)
+            .font(Font.largeTitle)
+            Button(action: {
+                
+                withAnimation(.easeInOut) {
+                    viewModel.resetGame()}
+                
+            }, label: {Text("New Game")})
+        }
     }
 }
 
@@ -35,17 +45,42 @@ struct CardView: View {
         }
     }
     
+    
+    @State private var animatedBonusRemaining: Double = 0
+    
+    private func startBonusTimeAnimation() {
+        animatedBonusRemaining = card.bonusRemaining
+        withAnimation(.linear(duration: card.bonusTimeRemaining)) {
+            animatedBonusRemaining = 0
+        }
+    }
+    
     @ViewBuilder
     private func body(for size: CGSize) -> some View {
         if card.isFaceUp || !card.isMatched {
-        ZStack {
-            Pie(startAngle: Angle.degrees(0-90), endAngle: Angle.degrees(110-90), clockwise: true)
-                .padding(5).opacity((0.4))
-            Text(card.content)
-                .font(Font.system(size: fontSize(for: size)))
+            ZStack {
+                Group {
+                    if card.isConsumingBonusTime {
+                        Pie(startAngle: Angle.degrees(0-90), endAngle: Angle.degrees(-animatedBonusRemaining*360-90), clockwise: true)
+                        
+                            .onAppear{
+                                self.startBonusTimeAnimation()
+                            }
+                    } else {
+                        Pie(startAngle: Angle.degrees(0-90), endAngle: Angle.degrees(-card.BonusRemaining*360-90), clockwise: true)
+                        
+                    }
+                }
+                .padding(5).opacity(0.4)
+                Text(card.content)
+                    .font(Font.system(size: fontSize(for: size)))
+                    .rotationEffect(Angle.degrees(card.isMatched ? 360 : 0))
+                    .animation(card.isMatched ? Animation.linear(duration: 1).repeatForever(autoreverses: false) : .default)
+            }
+            .cardify(isFaceUp: card.isFaceUp)
+            .transition(AnyTransition.scale)
         }
-        .cardify(isFaceUp: card.isFaceUp)
-        }
+        
     }
     
     // MARK: - Drawing Constants
@@ -55,7 +90,7 @@ struct CardView: View {
         min(size.width, size.height) * fontScaleFactor
     }
 }
-    
+
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         let game = EmojiMemoryGame()
